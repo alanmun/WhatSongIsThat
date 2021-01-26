e63120c5dd8281001fc5dd4b6d23cffd2e3d1af4
alanmun <aem108@pitt.edu>
2021-01-25 23:35:17 -0500

    Split javascript and html into different files for cleanliness

---
+++ script.js
@@ -0,0 +1,18 @@
+let params = new URLSearchParams(location.search);
+var songname = params.get('s'); //Extract song name
+var songartist = params.get('a'); //Extract artist name
+var songyoutube = params.get('y'); //Youtube url hash
+
+if (songyoutube === null) {
+  //document.getElementById("result").style.visibility = "hidden";
+}
+else {
+  songname = "     " + songname
+  songartist = "     " + songartist
+  songyoutube = "https://youtube.com/embed/" + songyoutube;
+}
+document.getElementById("song").innerHTML = songname; //songname;
+document.getElementById("artist").innerHTML = songartist; //songartist;
+document.getElementById("youtube").src = songyoutube;
+
+//alert(location.search);
