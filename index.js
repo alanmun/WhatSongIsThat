10ddb1a125268dd8c931d3f12673e3b3640d4e46
alanmun <aem108@pitt.edu>
2021-02-21 14:15:13 -0500

    Improved home page and added more info for users

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 let params = new URLSearchParams(location.search);
 var songname = params.get('s'); //Extract song name
 var songartist = params.get('a'); //Extract artist name
-var songyoutube = params.get('y'); //Youtube url hash
+var songyoutube = params.get('y'); //Youtube url video ID
 
 if (songyoutube === null) {
-  document.getElementById("result").style.visibility = "hidden";
+	document.getElementById("result").style.visibility = "hidden";
 }
 else {
-  songname = "     " + songname
-  songartist = "     " + songartist
-  songyoutube = "https://youtube.com/embed/" + songyoutube;
+	songname = "     " + songname
+	songartist = "     " + songartist
+	songyoutube = "https://youtube.com/embed/" + songyoutube;
+
+	document.getElementById("song").innerHTML = songname;
+	document.getElementById("artist").innerHTML = songartist;
+	document.getElementById("youtube").src = songyoutube;
+
+	document.getElementById("showOnlyOnHome").style.visibility = "hidden";
 }
-document.getElementById("song").innerHTML = songname; //songname;
-document.getElementById("artist").innerHTML = songartist; //songartist;
-document.getElementById("youtube").src = songyoutube;
